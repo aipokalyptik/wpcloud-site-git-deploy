b5068c9da82e2718ee679d6da268342f3d0af76d
aipokalyptik <demitrious.kelly+aipokalyptik@gmail.com>
2026-06-15 17:40:20 -0700

    Release v1.1.2

--- tests/test_auth_doctor.sh
+++ tests/test_auth_doctor.sh
@@ -88,7 +88,7 @@ export PATH="$fake_bin:$PATH"
 export WPCLOUD_TEST_GIT_LOG="$tmpdir/git.log"
 export WPCLOUD_TEST_SSH_LOG="$tmpdir/ssh.log"
 
-[[ "$("$cli" --version)" == "1.1.1" ]] || fail "--version should report current release line"
+[[ "$("$cli" --version)" == "1.1.2" ]] || fail "--version should report current release line"
 
 HOME="$home_dir" "$cli" init site \
   --repo https://github.com/example/private-site.git \
